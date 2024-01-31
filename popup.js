f31b8a6ee7527452f7c1da3a1ac44ce9c35578c3
Eric H <hyweric@gmail.com>
2024-01-31 09:22:43 -0800

    logo update for chrome web store application

--- popup.js
+++ popup.js
@@ -1,23 +1,25 @@
 document.getElementById('toggleButton').addEventListener('change', function() {
-    let toggleSwitch = document.getElementById('toggleButton');
-    let statusLabel = document.getElementById('statusLabel');
-  
-    chrome.storage.sync.get('enabled', function(data) {
-      let enabled = !data.enabled; // toggle
-      chrome.storage.sync.set({enabled: enabled});
-  
-      toggleSwitch.checked = enabled; // pos update
-      statusLabel.textContent = enabled ? 'Enabled' : 'Disabled'; // update label
+        let toggleSwitch = document.getElementById('toggleButton');
+        let statusLabel = document.getElementById('statusLabel');
+    
+        chrome.storage.sync.get('enabled', function(data) {
+            let enabled = !data.enabled; // toggle
+            chrome.storage.sync.set({enabled: enabled});
+    
+            toggleSwitch.checked = enabled; //pos of switch
+            statusLabel.textContent = enabled ? 'Enabled' : 'Disabled'; //status symbol text 
+            statusLabel.style.color = enabled ? '#00CC66' : 'red'; //status symbol color 
+        });
     });
-  });
   
   window.onload = function() {
     let toggleSwitch = document.getElementById('toggleButton');
     let statusLabel = document.getElementById('statusLabel');
   
     chrome.storage.sync.get('enabled', function(data) {
-      toggleSwitch.checked = data.enabled; //pos of switch
-      statusLabel.textContent = data.enabled ? 'Enabled' : 'Disabled'; //label
+      toggleSwitch.checked = data.enabled; 
+      statusLabel.textContent = data.enabled ? 'Enabled' : 'Disabled'; 
+      statusLabel.style.color = data.enabled ? '#00CC66' : 'red'; 
     });
   };
   
